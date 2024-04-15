56ecb12e5b048bcf0ba58f9894367488c031a748
LocDybala11 <locdybala11.it@gmail.com>
2024-04-15 23:33:22 +0700

    thay doi giao dien

--- resources/views/backend/contact/update.blade.php
+++ resources/views/backend/contact/update.blade.php
@@ -10,76 +10,79 @@
             background-color: #5bc0de;
         }
     </style>
-    <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Trang chủ/</span> Sửa thông tin website</h4>
-
-        <!-- Basic Layout & Basic with Icons -->
-        <div class="row">
-            <!-- Basic Layout -->
-            <div class="col-xxl">
-                <div class="card mb-12">
-                    <div class="card-header d-flex align-items-center justify-content-between">
-                        <h5 class="mb-0">Sửa thông tin website</h5>
-                    </div>
-                    @php
-                        $success=Session::get('success');
-                        if($success){
-                        echo '<div class="alert alert-success">
-                             '.$success.'
-                           </div>';
-                        Session::put('success', null);}
-                    @endphp
-                    <div class="card-body">
-                        <form action="{{route('update_info',['id'=>$contact->info_id])}}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-default-name">Thông tin liên hệ</label>
-                                <div class="col-sm-10">
-                                    <textarea
-                                        id="ckeditor"
-                                        class="form-control" name="info_name"
-                                        placeholder=""
-                                    >{!! $contact->info_contact !!}</textarea>
-                                </div>
+    <section class="pcoded-main-container">
+        <div class="pcoded-content">
+            <!-- [ breadcrumb ] start -->
+            <div class="page-header">
+                <div class="page-block">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="page-header-title">
+                                <h5 class="m-b-10">Thông tin website</h5>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-default-message">Bản đồ</label>
-                                <div class="col-sm-10">
-                                  <textarea rows="6" cols="60"
-                                      class="form-control" name="info_map"
-                                      placeholder="Mô tả sản phẩm"
-                                  >{{ $contact->info_map }}</textarea>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-default-name">Ảnh Logo</label>
-                                <div class="col-sm-10">
-                                    <input type="file" class="form-control" id="basic-default-name" name="image"
-                                    />
-                                    <img class="input-rounded mt-2" src="{{ URL::to('/upload/info/'.$contact->info_image) }}" height="150" width="150" alt="">
-
-                                </div>
-                            </div>
-
-
-                            <div class="row justify-content-end">
-                                <div class="col-sm-10">
-                                    <button type="submit" class="btn btn-primary">Sửa</button>
-                                </div>
-                            </div>
-                        </form>
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{route('dashboard')}}"><i class="feather icon-home"></i></a></li>
+                                <li class="breadcrumb-item"><a href="{{route('add_infomation')}}">Sửa thông tin website</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
+            <!-- [ breadcrumb ] end -->
+            <!-- [ Main Content ] start -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>Sửa thông tin website</h5>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{route('update_info',['id'=>$contact->info_id])}}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label class="floating-label" for="ckeditor">Thông tin liên hệ</label>
+                                            <textarea
+                                                id="ckeditor"
+                                                class="form-control" name="info_name"
+                                                placeholder=""
+                                            >{!! $contact->info_contact !!}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label class="floating-label" for="Text">Bản đồ</label>
+                                            <textarea rows="6" cols="60"
+                                                      class="form-control" name="info_map"
+                                                      placeholder="Mô tả sản phẩm"
+                                            >{{ $contact->info_map }}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <label class="floating-label" for="password">Ảnh Logo</label>
+                                            <input type="file" class="form-control" id="basic-default-name" name="image"
+                                            />
+                                            <img class="input-rounded mt-2" src="{{ URL::to('/upload/info/'.$contact->info_image) }}" height="150" width="150" alt="">
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <button type="submit" class="btn  btn-primary">Sửa</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- [ Main Content ] end -->
+
         </div>
-        <!-- Basic with Icons -->
-    </div>
-    </div>
+    </section>
 @endsection
 @section('js')
     <script>
         CKEDITOR.replace('ckeditor');
-        CKEDITOR.replace('ckeditor1');
 
     </script>
 @endsection
