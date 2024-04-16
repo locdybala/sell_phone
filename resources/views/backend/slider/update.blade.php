1cc5a9fda6464d5048cf529234c90ffce34a211a
LocDybala11 <locdybala11.it@gmail.com>
2024-04-16 22:12:52 +0700

    thay doi giao dien admin

--- resources/views/backend/slider/update.blade.php
+++ resources/views/backend/slider/update.blade.php
@@ -1,57 +1,80 @@
 @extends('backend.admin_layout')
 @section('content')
-    <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Trang chủ/</span> Thêm slider</h4>
-
-        <!-- Basic Layout & Basic with Icons -->
-        <div class="row">
-            <!-- Basic Layout -->
-            <div class="col-xxl">
-                <div class="card mb-12">
-                    <div class="card-header d-flex align-items-center justify-content-between">
-                        <h5 class="mb-0">Thêm slider</h5>
-                    </div>
-                    <div class="card-body">
-                        <form action="{{  route('update_slider',['id'=>$slider->slider_id]) }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-default-name">Tên slider</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" value="{{ $slider->slider_name }}" id="basic-default-name" name="name"
-                                    />
-                                </div>
+    <section class="pcoded-main-container">
+        <div class="pcoded-content">
+            <!-- [ breadcrumb ] start -->
+            <div class="page-header">
+                <div class="page-block">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="page-header-title">
+                                <h5 class="m-b-10">Quản lý slider</h5>
                             </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-default-name">Ảnh</label>
-                                <div class="col-sm-10">
-                                    <input type="file" class="form-control" id="basic-default-name" name="image"
-                                    />
-                                    <img class="input-rounded mt-2" src="{{ URL::to('/upload/slider/'.$slider->slider_image) }}" height="150" width="150" alt="">
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label class="col-sm-2 col-form-label" for="basic-default-message">Mô tả</label>
-                                <div class="col-sm-10">
-                                  <textarea
-                                      id="ckeditor"
-                                      class="form-control" name="description"
-                                      placeholder="Mô tả slider"
-                                  >{{ $slider->slider_desc }}</textarea>
-                                </div>
-                            </div>
-                            <div class="row justify-content-end">
-                                <div class="col-sm-10">
-                                    <button type="submit" class="btn btn-primary">Sửa</button>
-                                </div>
-                            </div>
-                        </form>
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{route('dashboard')}}"><i
+                                            class="feather icon-home"></i></a></li>
+                                <li class="breadcrumb-item"><a href="#!">Slider</a></li>
+                                <li class="breadcrumb-item"><a href="#!">Sửa slider</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
+            <!-- [ breadcrumb ] end -->
+            <!-- [ Main Content ] start -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>Sửa slider</h5>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{  route('update_slider',['id'=>$slider->slider_id]) }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label class="floating-label" for="name">Tên slider</label>
+                                            <input type="text" value="{{ $slider->slider_name }}" class="form-control" id="basic-default-name" name="name"
+                                                   placeholder="Tên ảnh"/>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label class="floating-label" for="image">Ảnh</label>
+                                            <input type="file" class="form-control" id="basic-default-name" name="image"
+                                            />
+                                            <img class="input-rounded mt-2" src="{{ URL::to('/upload/slider/'.$slider->slider_image) }}" height="150" width="150" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label class="floating-label" for="description">Mô tả</label>
+                                            <textarea
+                                                id="ckeditor"
+                                                class="form-control" name="description"
+                                                placeholder="Mô tả slider"
+                                            >{!!  $slider->slider_desc !!}</textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary">Sửa</button>
+                                            <a href="/admin/slider/all_slider" class="btn btn-default">Huỷ</a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- [ form-element ] start -->
+            </div>
+            <!-- [ Main Content ] end -->
+
         </div>
-        <!-- Basic with Icons -->
-    </div>
-    </div>
+    </section>
 @endsection
 @section('js')
     <script>
