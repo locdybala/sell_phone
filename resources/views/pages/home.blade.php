69cfddac72f2a3ceda6eb6734ac4968829e95675
LocDybala11 <locdybala11.it@gmail.com>
2024-06-03 21:14:22 +0700

    add code

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -60,38 +60,6 @@
             </div>
         </div>
     </section>
-    <!--  New Product End -->
-    <div style="padding-bottom: 50px;" class="gallery-area">
-        <div class="container-fluid p-0 fix">
-            <div class="row">
-                <div class="col-xl-6 col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-gallery mb-30">
-                        <div class="gallery-img big-img" style="background-image: url(frontend/assets/img/gallery/gallery1.png);"></div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                    <div class="single-gallery mb-30">
-                        <div class="gallery-img big-img" style="background-image: url(frontend/assets/img/gallery/gallery2.png);"></div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-4 col-md-12">
-                    <div class="row">
-                        <div class="col-xl-12 col-lg-12 col-md-6 col-sm-6">
-                            <div class="single-gallery mb-30">
-                                <div class="gallery-img small-img" style="background-image: url(frontend/assets/img/gallery/gallery3.png);"></div>
-                            </div>
-                        </div>
-                        <div class="col-xl-12 col-lg-12  col-md-6 col-sm-6">
-                            <div class="single-gallery mb-30">
-                                <div class="gallery-img small-img" style="background-image: url(frontend/assets/img/gallery/gallery4.png);"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
     <!-- ? New Product Start -->
     <div style="padding-bottom: 50px;" class="popular-items section-paddingt2">
         <div class="container">
